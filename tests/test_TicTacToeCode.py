aefb89b19b7f18868f4d501b0da0baa04013e656
OzYossarian <alex.townsend-teague@outlook.com>
2022-05-13 15:41:57 +0200

    Add TODO for remaining tic-tac-toe tests

--- tests/test_TicTacToeCode.py
+++ tests/test_TicTacToeCode.py
@@ -229,38 +229,39 @@ def test_create_borders():
             if borders_minus:
                 assert_plaquette_borders_correct(anchors, borders_minus, -1)
 
+# TODO: Remaining tests.
 
-def test_find_stabilized_plaquettes():
-    assert False
-
-
-def test_reinfer_plaquettes():
-    assert False
-
-
-def test_carry_over_plaquettes():
-    assert False
-
-
-def test_remove_plaquettes():
-    assert False
-
-
-def test_relearn_plaquettes():
-    assert False
-
-
-def test_time_within_repeating_part_of_code():
-    assert False
-
-
-def test_plan_detectors():
-    assert False
-
-
-def test_plan_detectors_of_type():
-    assert False
-
-
-def test_create_detectors():
-    assert False
+# def test_find_stabilized_plaquettes():
+#     assert False
+#
+#
+# def test_reinfer_plaquettes():
+#     assert False
+#
+#
+# def test_carry_over_plaquettes():
+#     assert False
+#
+#
+# def test_remove_plaquettes():
+#     assert False
+#
+#
+# def test_relearn_plaquettes():
+#     assert False
+#
+#
+# def test_time_within_repeating_part_of_code():
+#     assert False
+#
+#
+# def test_plan_detectors():
+#     assert False
+#
+#
+# def test_plan_detectors_of_type():
+#     assert False
+#
+#
+# def test_create_detectors():
+#     assert False
