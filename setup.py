2c400cfe8f892ceeabedb3104038706222685690
Peter-Jan <peter-janderks@hotmail.com>
2022-11-11 16:06:02 +0100

    start work on making code compatible with python 3.9

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    python_requires='>=3.6',
-    name='FaultTolerantQPU',
-    version='0.1dev',
+    python_requires='>=3.9',
+    name='Kandel',
+    version='0.2dev',
     packages=['main', 'tests'],
     license='GNU General Public License v3.0',
     long_description=open('README.md').read(),
