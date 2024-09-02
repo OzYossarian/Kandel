25946b4c61ffb055a2cdf31afa20a77c81e2f430
peter-janderks <peter-janderks@hotmail.com>
2024-09-02 14:43:28 +0200

    added stability observable for floquet colour

--- tests/codes/tic_tac_toe/test_FloquetColourCode.py
+++ tests/codes/tic_tac_toe/test_FloquetColourCode.py
@@ -1,4 +1,6 @@
 import webbrowser
+from main.building_blocks.pauli import Pauli
+from main.building_blocks.pauli.PauliLetter import PauliLetter
 from main.codes.tic_tac_toe.FloquetColourCode import FloquetColourCode
 
 from main.building_blocks.detectors.Stabilizer import Stabilizer
@@ -9,42 +11,57 @@ from main.compiling.syndrome_extraction.extractors.ancilla_per_check.mixed.CxCyC
 import stim
 
 
-def generate_circuit_X_observable(rounds, distance):
-    code = FloquetColourCode(distance)
-    logical_observables = [code.logical_qubits[1].x]
-    initial_stabilizers = []
-    for check in code.check_schedule[0]:
-        initial_stabilizers.append(Stabilizer([(0, check)], 0))
+def generate_circuit(code, rounds, observable_type, check_schedule_index, stability_operator=None, final_measurements=None):
+    if stability_operator:
+        logical_observables = [stability_operator]
+
+    elif observable_type == 'z':
+        logical_observables = [code.logical_qubits[0].z]
+    elif observable_type == 'x':
+        logical_observables = [code.logical_qubits[1].x]
+
+    initial_stabilizers = [Stabilizer(
+        [(0, check)], 0) for check in code.check_schedule[check_schedule_index]]
 
     compiler = AncillaPerCheckCompiler(
-        noise_model=PhenomenologicalNoise(1, 1),
-        syndrome_extractor=CxCyCzExtractor())
+        noise_model=PhenomenologicalNoise(0.1, 0.1),
+        syndrome_extractor=CxCyCzExtractor()
+    )
+
     stim_circuit = compiler.compile_to_stim(
         code=code,
         total_rounds=rounds,
         initial_stabilizers=initial_stabilizers,
-        observables=logical_observables
+        observables=logical_observables,
+        final_measurements=final_measurements
     )
-    return (stim_circuit)
+
+    return stim_circuit
+
+
+def generate_circuit_X_observable(rounds, distance):
+    code = FloquetColourCode(distance)
+    return generate_circuit(code, rounds, 'x', 0)
 
 
 def generate_circuit_Z_observable(rounds, distance):
     code = FloquetColourCode(distance)
-    logical_observables = [code.logical_qubits[0].z]
-    initial_stabilizers = []
-    for check in code.check_schedule[1]:
-        initial_stabilizers.append(Stabilizer([(0, check)], 0))
+    return generate_circuit(code, rounds, 'z', 1)
 
-    compiler = AncillaPerCheckCompiler(
-        noise_model=PhenomenologicalNoise(1, 1),
-        syndrome_extractor=CxCyCzExtractor())
-    stim_circuit = compiler.compile_to_stim(
-        code=code,
-        total_rounds=rounds,
-        initial_stabilizers=initial_stabilizers,
-        observables=logical_observables
-    )
-    return (stim_circuit)
+
+def generate_Z_stability_circuit(rounds, distance):
+    code = FloquetColourCode(distance)
+    final_measurements = [Pauli(qubit, PauliLetter(
+        'X')) for qubit in code.data_qubits.values()]
+
+    return generate_circuit(code, rounds, None, 0, stability_operator=code.z_stability_operator, final_measurements=final_measurements)
+
+
+def generate_X_stability_circuit(rounds, distance):
+    code = FloquetColourCode(distance)
+    final_measurements = [Pauli(qubit, PauliLetter(
+        'Z')) for qubit in code.data_qubits.values()]
+    return generate_circuit(code, rounds, None, 1, stability_operator=code.x_stability_operator, final_measurements=final_measurements)
 
 
 def check_for_5_detectors_violated(dem):
@@ -66,6 +83,8 @@ def check_parity_of_number_of_violated_detectors(circuit: stim.Circuit):
 
 
 def check_distance(circuit: stim.Circuit, distance):
+    print(len(circuit.detector_error_model(
+        approximate_disjoint_errors=True).shortest_graphlike_error()))
     assert len(circuit.detector_error_model(
         approximate_disjoint_errors=True).shortest_graphlike_error()) == distance
 
@@ -89,11 +108,36 @@ def test_properties_of_d4_codes():
         check_distance(circuit, 4)
 
 
-def test_Z_observable():
+def test_Z_observable_d4():
     circuit: stim.Circuit = generate_circuit_Z_observable(12, 4)
     check_parity_of_number_of_violated_detectors(circuit)
     check_n_detectors(circuit, 12)
     check_distance(circuit, 4)
 
 
+def test_Z_observable_d8():
+    circuit: stim.Circuit = generate_circuit_Z_observable(24, 8)
+    check_parity_of_number_of_violated_detectors(circuit)
+    check_distance(circuit, 8)
 
+
+def test_Z_stability_experiment():
+    circuit: stim.Circuit = generate_Z_stability_circuit(16, 4)
+    check_distance(circuit, 4)
+
+    circuit: stim.Circuit = generate_Z_stability_circuit(12, 4)
+    check_distance(circuit, 3)
+
+    circuit: stim.Circuit = generate_Z_stability_circuit(20, 4)
+    check_distance(circuit, 5)
+
+
+def test_X_stability_experiment():
+    circuit: stim.Circuit = generate_X_stability_circuit(16, 4)
+    check_distance(circuit, 4)
+
+    circuit: stim.Circuit = generate_X_stability_circuit(12, 4)
+    check_distance(circuit, 3)
+
+    circuit: stim.Circuit = generate_X_stability_circuit(20, 4)
+    check_distance(circuit, 5)
