7db478d49ba758d972164ebc228c0a20778e4f71
peter-janderks <peter-janderks@hotmail.com>
2024-09-04 11:23:33 +0200

    refactor test_HoneycombCode.py

--- tests/codes/tic_tac_toe/test_HoneycombCode.py
+++ tests/codes/tic_tac_toe/test_HoneycombCode.py
@@ -8,48 +8,48 @@ import stim
 from main.utils.enums import State
 
 
-def generate_circuit(rounds, distance):
+def generate_circuit(rounds: int, distance: int, observable_type: str = 'X') -> stim.Circuit:
+    """Generates a quantum error correction circuit for the Honeycomb code.
+
+    Args:
+        rounds (int): The number of rounds for the circuit.
+        distance (int): The distance of the Honeycomb code.
+        observable_type (str): The type of logical observable ('X' or 'Z'). Defaults to 'X'.
+
+    Returns:
+        Any: The compiled stim circuit.
+    """
     code = HoneycombCode(distance)
-    logical_observables = [code.logical_qubits[1].x]
-    initial_stabilizers = []
-    for check in code.check_schedule[0]:
-        initial_stabilizers.append(Stabilizer([(0, check)], 0))
+
+    if observable_type == 'X':
+        logical_observables = [code.logical_qubits[1].x]
+        initial_stabilizers = [Stabilizer([(0, check)], 0)
+                               for check in code.check_schedule[0]]
+        initial_states = None
+        noise_model = PhenomenologicalNoise(1, 1)
+    elif observable_type == 'Z':
+        logical_observables = [code.logical_qubits[0].z]
+        initial_stabilizers = []
+        initial_states = {qubit: State(0)
+                          for qubit in code.data_qubits.values()}
+        noise_model = PhenomenologicalNoise(0.1, 0.1)
+    else:
+        raise ValueError("observable_type must be 'X' or 'Z'")
 
     compiler = AncillaPerCheckCompiler(
-        noise_model=PhenomenologicalNoise(1, 1),
-        syndrome_extractor=CxCyCzExtractor())
+        noise_model=noise_model,
+        syndrome_extractor=CxCyCzExtractor()
+    )
+
     stim_circuit = compiler.compile_to_stim(
         code=code,
         total_rounds=rounds,
         initial_stabilizers=initial_stabilizers,
-        observables=logical_observables
-    )
-    return (stim_circuit)
-
-
-def generate_circuit_Z_observable(rounds, distance):
-
-    code = HoneycombCode(distance)
-    logical_observables = [code.logical_qubits[0].z]
-    initial_states = dict()
-    for qubit in code.data_qubits.values():
-
-        #        check = Check([Pauli(qubit, letter=PauliLetter("Z"))])
-        initial_states[qubit] = State(0)
-#        initial_stabilizers.append(Stabilizer([(0, check)], 0))
-#    for check in code.check_schedule[gauge_factors[0]+gauge_factors[1]]:
-#           initial_stabilizers.append(Stabilizer([(0, check)], 0))
-
-    compiler = AncillaPerCheckCompiler(
-        noise_model=PhenomenologicalNoise(0.1, 0.1),
-        syndrome_extractor=CxCyCzExtractor())
-    stim_circuit = compiler.compile_to_stim(
-        code=code,
-        total_rounds=rounds,
         initial_states=initial_states,
         observables=logical_observables
     )
-    return (stim_circuit)
+
+    return stim_circuit
 
 
 def check_for_5_detectors_violated(dem):
@@ -58,11 +58,11 @@ def check_for_5_detectors_violated(dem):
             error_targets = dem_instruction.targets_copy()
             n_violated_detectors = 0
             for target in error_targets:
-                if target.is_relative_detector_id() == True:
+                if target.is_relative_detector_id():
                     n_violated_detectors += 1
             if n_violated_detectors == 5:
-                return (False)
-    return (True)
+                return False
+    return True
 
 
 def check_parity_of_number_of_violated_detectors_d4(circuit: stim.Circuit):
@@ -77,13 +77,13 @@ def check_distance(circuit: stim.Circuit, distance):
 
 def test_properties_of_d4_codes():
     for n_rounds in range(12, 24):
-
-        circuit: stim.Circuit = generate_circuit(n_rounds, 4)
+        circuit: stim.Circuit = generate_circuit(n_rounds, 4, 'X')
         check_parity_of_number_of_violated_detectors_d4(circuit)
         check_distance(circuit, 4)
 
 
 def test_z_obs():
-    circuit: stim.Circuit = generate_circuit_Z_observable(12, 4)
+    circuit: stim.Circuit = generate_circuit(
+        n_rounds=12, distance=4, observable_type='Z')
     check_parity_of_number_of_violated_detectors_d4(circuit)
     check_distance(circuit, 4)
