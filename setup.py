e805f488b48ddf173e1dafff6a0ccbc119d2e270
OzYossarian <38085891+OzYossarian@users.noreply.github.com>
2025-02-04 23:53:58 +0100

    Yet more setup.py tweaks.

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     packages=find_packages(include=['main', 'main.*']),
     license='GNU General Public License v3.0',
     long_description=open('README.md').read(),
-    install_requires=["stim", "stimcirq"]
+    install_requires=["stim", "stimcirq", "alive_progress"]
 )
