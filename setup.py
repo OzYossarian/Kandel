84fda6f7618622e6ddf29a4f70c15326c3cb041e
OzYossarian <38085891+OzYossarian@users.noreply.github.com>
2025-02-04 23:52:21 +0100

    More setup.py tweaking

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     packages=find_packages(include=['main', 'main.*']),
     license='GNU General Public License v3.0',
     long_description=open('README.md').read(),
-    install_requires=["numpy"]
+    install_requires=["stim", "stimcirq"]
 )
